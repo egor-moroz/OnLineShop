baadff7084f49979873f63862b391ef8ff56e765
egor-moroz <egorlex@gmail.com>
2017-03-17 22:18:25 +0200

    update Account class

--- src/main/java/com/shop/model/Account.java
+++ src/main/java/com/shop/model/Account.java
@@ -1,9 +1,37 @@
 package com.shop.model;
 
+import java.io.Serializable;
+
 /**
  * Created by EgorPC on 17.03.2017.
  */
-public class Account {
+public class Account implements Serializable{
 
+    public String userName;
+    private String password;
+    private boolean active;
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
