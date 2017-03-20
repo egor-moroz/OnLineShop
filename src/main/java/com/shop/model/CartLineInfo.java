09d2a400bf946ff4b9f2a7a370b484267efd86da
egor-moroz <egorlex@gmail.com>
2017-03-20 17:52:36 +0200

    update CartLineInfo class

--- src/main/java/com/shop/model/CartLineInfo.java
+++ src/main/java/com/shop/model/CartLineInfo.java
@@ -4,5 +4,31 @@ package com.shop.model;
  * Created by EgorPC on 18.03.2017.
  */
 public class CartLineInfo {
+    private ProductInfo productInfo;
+    private int quantity;
+
+    public CartLineInfo() {
+        this.quantity = 0;
+    }
+
+    public ProductInfo getProductInfo() {
+        return productInfo;
+    }
+
+    public void setProductInfo(ProductInfo productInfo) {
+        this.productInfo = productInfo;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getAmount() {
+        return this.productInfo.getPrice() * this.quantity;
+    }
 
 }
